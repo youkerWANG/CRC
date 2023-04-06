a070e5ebfc9bb0731cafd10cdc40d6db40642b0c
xinyaowang <791157327nn@gmail.com>
2023-04-06 19:55:14 +0900

    fix 01

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/user_photos'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template("index.html")
+
+
+@app.route('/upload_photos', methods=["POST", "GET"])
+def photos():
+    files = request.files.getlist('photo')
+
+    for i in files:
+        filename = i.filename
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        i.save(filepath)
+
+    return redirect(url_for('index', _anchor='upupload'))
+
+
+@app.route('/upload_option', methods=['POST'])
+def options():
+    elected_options = request.form.getlist('options')
+    print(elected_options)
+    return redirect(url_for('index', _anchor='upupload'))
 
 
 if __name__ == '__main__':
